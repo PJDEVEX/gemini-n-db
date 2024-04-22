b4fb17ead8632d5c8ea7b04f43b83325dd353c97
PJDEVEX <piyankara.jayadewa@gmail.com>
2024-04-22 19:23:14 +0200

    chor: Add docstrings

--- src/gcp/gcp_confi.py
+++ src/gcp/gcp_confi.py
@@ -18,9 +18,17 @@ PROJECT_ID = os.getenv("PROJECT_ID")
 REGION = os.getenv("REGION")
 
 
-def get_service_account_credentials_and_refresh(key_file) -> Credentials:
+def get_service_account_credentials_and_refresh(
+    key_file,
+) -> Credentials:
     """
-    Get the service account credentials & refresh
+    This function is used to get and refresh the service account credentials.
+
+    Parameters:
+    key_file (str): The path to the service account file.
+
+    Returns:
+    Credentials: The refreshed credentials object.
     """
     try:
         credentials = Credentials.from_service_account_file(
@@ -33,21 +41,40 @@ def get_service_account_credentials_and_refresh(key_file) -> Credentials:
     except Exception as e:
         # TODO: Add api error logging (f"Internal Server Error and return 500")
         print(f"get service account credentials error: {e}")
-        raise RuntimeError("Failed to load service acccount credentials") from e
+        raise RuntimeError(
+            "Failed to load service acccount credentials"
+        ) from e
+
 
 # Call get_service_account_credentials_and_refresh
 key_file = GOOGLE_APPLICATION_CREDENTIALS
 credentials = get_service_account_credentials_and_refresh(key_file)
 
-def initialize_vertexai_client(project_id: str, region: str, credentials: Credentials):
+
+def initialize_vertexai_client(
+    project_id: str, region: str, credentials: Credentials
+):
     """
-    Initialize the Vertex AI client
+    This function is used to initialize the Vertex AI client.
+
+    Parameters:
+    project (str): The ID of your Google Cloud project.
+    location (str): The region where your resources are located.
+
+    Returns:
+    aiplatform.gapic.PipelineServiceClient: The initialized client object.
     """
     try:
         client = vertexai.init(project=PROJECT_ID)
         print("Vertex AI client initialized successfully!")
         return client
     except Exception as e:
-        raise RuntimeError("Failed to initialize Vertex AI client") from e
-    
-vertexai_client = initialize_vertexai_client(PROJECT_ID, REGION, credentials)
+        raise RuntimeError(
+            "Failed to initialize Vertex AI client"
+        ) from e
+
+
+# Call initialize_vertexai_client
+vertexai_client = initialize_vertexai_client(
+    PROJECT_ID, REGION, credentials
+)
