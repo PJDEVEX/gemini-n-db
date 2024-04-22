fdafb2ae84d29369583f38491bcc1d54e9f1d3e5
PJDEVEX <piyankara.jayadewa@gmail.com>
2024-04-22 20:17:38 +0200

    feat: add generate content using the gen model

--- src/gcp/gcp_confi.py
+++ src/gcp/gcp_confi.py
@@ -78,3 +78,28 @@ def initialize_vertexai_client(
 vertexai_client = initialize_vertexai_client(
     PROJECT_ID, REGION, credentials
 )
+
+def get_gemini_response(question, prompt):
+    """
+    This function is used to generate content using the generative model.
+
+    Parameters:
+    question (str): The question to be answered.
+    prompt (str): The prompt for the model.
+
+    Returns:
+    str: The generated content.
+    """
+    try:
+        model =GenerativeModel(MODEL)
+    except ImportError as e:
+        print(f"Error: {e}")
+        raise RuntimeError("Failed to load the model") from e
+    try:
+        response = model.generate_content([question, prompt])
+    except Exception as e:
+        print(f"Error: {e}")
+        raise RuntimeError("Failed to generate content") from e
+    return response.text
+
+
